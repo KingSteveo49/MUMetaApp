8bc4c672d238fe2b30f2b54401492fb1cbfd87c9
sdmiller2015 <sdmiller2015@D101198.manchester.edu>
2015-03-03 18:41:05 -0500

    Initializeing project.

---
+++ MUMetaApp/src/mumetaapp/Event.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mumetaapp;
+
+/**
+ *
+ * @author sdmiller2015
+ */
+public interface Event {
+    
+}
