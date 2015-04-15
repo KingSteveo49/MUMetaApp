61fc402230184ba3e8e9e75b48b7273c0f044da9
unknown <sdmiller2015@MU000657.manchester.edu>
2015-04-15 14:23:08 -0400

    Got the tree view working. Now waiting for controller

--- MUMetaApp/src/fxmlexample/GUIController.java
+++ MUMetaApp/src/fxmlexample/GUIController.java
@@ -3,6 +3,8 @@ package fxmlexample;
 import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 import java.io.File;
 import java.io.IOException;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -11,6 +13,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
@@ -19,7 +23,6 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -36,34 +39,34 @@ public class GUIController {
     @FXML 
     TextArea feedBack;
     @FXML
-    GridPane optionsGridPane;
-//    @FXML
-//    private GridPane rootElement; // this is my root element in FXML (fx:id="borderPane")
+    TreeView contentTreeView;
+    @FXML
+    GridPane workAreaGridPane;
+    
     
     @FXML
     protected void handleKeyInput(ActionEvent event){
         
     }
-    
     @FXML
     protected void handleNewAction(ActionEvent event){
         
     }
-    
     @FXML
     protected void handleOpenAction(ActionEvent event) throws IOException{
         Controller.getInstance().manageAction(new Action("open", null));
     }
-    
+    @FXML
+    protected void handleTestOpenAction(ActionEvent event) throws IOException, SAXException{
+        displayFile();
+    }
     @FXML
     protected void handleSaveAction(ActionEvent event){
         displayFeedback(new Action(null, "User wants to save"));
     }
-    
     @FXML
     protected void handleSaveAsAction(ActionEvent event){
     }
-    
     @FXML
     protected void handleExitAction(ActionEvent event){
     }
@@ -73,7 +76,7 @@ public class GUIController {
     }
     @FXML 
     protected void displayFeedback(String s) {
-        feedBack.appendText(s+"\n");
+        feedBack.appendText(s);
     }
     
     @FXML
@@ -94,40 +97,86 @@ public class GUIController {
         file = fileChooser.showOpenDialog(stage);
         Controller.getInstance().manageAction(new Action("fileLocationChosen", file.getPath()));
 }
+    
     @FXML
     private void displayFile() throws SAXException, IOException{
         System.out.println("The GUI is now attempting to display the file");
-        
+        //--This is test code used to initialize a fake xml document that will
+        //--be used to resemble the one that would be passed in
         DOMParser parser = new DOMParser();
-        parser.parse("C:\\Users\\Stephen\\Desktop\\books.xml");
+        parser.parse("H:\\books.xml");
         Document dom = parser.getDocument();
-        walkDOM(dom);
+        //----------------------------------------------------------------------
+        TreeItem rootItem = new TreeItem("Categories");
+        rootItem.setExpanded(true);
         
+        contentTreeView.setRoot(rootItem);
+        contentTreeView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem<String>>() {
+
+                @Override
+                public void changed(ObservableValue<? extends TreeItem<String>> observable,TreeItem<String> old_val, TreeItem<String> new_val) {
+                    TreeItem<String> selectedItem = new_val;
+                    displayFeedback("Selected Text : " + selectedItem.getValue()+"\n");
+                    // do what ever you want
+                }
+
+            });
+        
+        walkNode(dom.getDocumentElement(), contentTreeView.getRoot());
     }
     
     private void walkDOM(Document dom) {
-        NodeList e = dom.getElementsByTagName("tape");
+        Element n = dom.createElement("Thinker");
+        n.setAttribute("id", "unique");
+        System.out.println(n.getAttribute("id"));
         
-        for (int i = 0; i < e.getLength(); i++) {
-      Node aNode = e.item(i);
-      if (aNode.hasChildNodes())
-        walkNode(aNode);
-      else
-        displayFeedback(aNode.getTextContent());
     }
-     
-  }
-     private void walkNode(Node theNode) {
+    
+    private void walkNode(Node theNode, TreeItem rootItem) {
     NodeList children = theNode.getChildNodes();
-   //printNode(theNode);
+    TreeItem item = null;
+    String value = "NOT INITILIZED";
     for (int i = 0; i < children.getLength(); i++) {
-      Node aNode = children.item(i);
-      if (aNode.hasChildNodes())
-        walkNode(aNode);
-      else
-        displayFeedback(aNode.getTextContent());
+        Node aNode = children.item(i);
+        if (aNode.hasChildNodes()){
+            
+            value = aNode.getAttributes().getNamedItem("id").toString();
+            String delims = "[\"]+";
+            String[] splitString = value.split(delims);
+            value = splitString[1];
+            
+            item = new TreeItem(value);
+            //item = new TreeItem(aNode.getAttributes().getNamedItem("id").toString());
+            rootItem.getChildren().add(item);
+            rootItem.setExpanded(true);
+            
+            walkNode(aNode, item);
+      }
+        else{
+            displayFeedback(aNode.getTextContent());
+      }
     }
+    
   }
+    
+//    protected void testTreeView(){
+//        final Node rootIcon = new ImageView(new Image(getClass().getResourceAsStream("root.png")));
+//        final Image depIcon = new Image(getClass().getResourceAsStream("department.png"));
+//        List<Employee> employees = Arrays.<Employee>asList(
+//            new Employee("Ethan Williams", "Sales Department"),
+//            new Employee("Emma Jones", "Sales Department"),
+//            new Employee("Michael Brown", "Sales Department"),
+//            new Employee("Anna Black", "Sales Department"),
+//            new Employee("Rodger York", "Sales Department"),
+//            new Employee("Susan Collins", "Sales Department"),
+//            new Employee("Mike Graham", "IT Support"),
+//            new Employee("Judy Mayer", "IT Support"),
+//            new Employee("Gregory Smith", "IT Support"),
+//            new Employee("Jacob Smith", "Accounts Department"),
+//            new Employee("Isabella Johnson", "Accounts Department"));
+//        TreeItem<String> rootNode = new TreeItem<String>("MyCompany Human Resources", rootIcon);
+//
+//    }
     
     private int row = 0;
     
@@ -139,14 +188,12 @@ public class GUIController {
 
             @Override
             public void handle(MouseEvent event) {
-                displayFeedback(l.getId()+ " was pressed adding new label");
+                displayFeedback(l.getId()+ " was pressed, adding new label \n");
                 testyTest();
             }
         });
-    
-        optionsGridPane.add(l, 0, row);
+        //optionsGridPane.add(l, 0, row);
         row++;
-        
     }
     
     public void manageAction(Action a) throws IOException{
