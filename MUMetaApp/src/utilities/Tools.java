3e2d57faa0218e6cb79795356cfbcdb1dfdabc28
unknown <sdmiller2015@MU000471.manchester.edu>
2015-04-20 18:56:28 -0400

    Added app stuff

--- MUMetaApp/src/utilities/Tools.java
+++ MUMetaApp/src/utilities/Tools.java
@@ -7,12 +7,23 @@
 package utilities;
 
 import com.sun.org.apache.xerces.internal.parsers.DOMParser;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 //Export Test
 import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 /**
  *
@@ -20,7 +31,7 @@ import java.nio.file.Files;
  */
 public class Tools {
     
-    public static final String defaultAppDir = "DefaultApp";
+    public static final Path defaultAppDir = FileSystems.getDefault().getPath("DefaultApp", (String) null);
     
     public static boolean compare(String s1,String s2)
     {
@@ -50,9 +61,36 @@ public class Tools {
         }
     }
     
-    public static void exportApp(String targetLocation)
+    public static void saveDoc(Document document, String path)
+  {
+    try
     {
+      DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+      
+      TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      Transformer transformer = transformerFactory.newTransformer();
+      
+      DOMSource source = new DOMSource(document);
+      StreamResult result = new StreamResult(new File(path));
+      
+      transformer.transform(source, result);
+      
+      System.out.println("File saved!");
+    }
+    catch(ParserConfigurationException | TransformerException exc)
+    {
+      System.out.println(exc);
+    }
+  }
+    
+    public static void exportApp(Path targetLocation)
+    {
+        try{
         Files.copy(defaultAppDir,targetLocation);
         // TODO: Also throw the DOM xml stuff into the new location
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
